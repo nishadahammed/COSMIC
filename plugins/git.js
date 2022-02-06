b7ea6d66148873963235d703c3771d169787a20a
nishadahammed <64012089+nishadahammed@users.noreply.github.com>
2022-02-06 09:01:08 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,16 +7,16 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.ibb.co/KWyM3Qw/COSMIC.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/kF5SpFM.jpeg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `𝑪𝒐𝒔𝒎𝒊𝒄 𝒃𝒐𝒕 𝒄𝒓𝒆𝒂𝒕𝒆𝒅 𝒃𝒚 𝒂𝒏𝒔𝒉𝒊𝒅
 Creator number 👉: wa.me/916238560742
 
-Githublink (Setup)  👇:    https://github.com/ANUSER1/COSMIC
+Githublink (Setup)  👇:    https://github.com/nishadahammed/Miss-Aliza
 
-DEPLOYE  👇:  https://heroku.com/deploy?template=https://github.com/ANUSER1/COSMIC.git
+DEPLOYE  👇:  https://heroku.com/deploy?template=https://github.com/nishadahammed/Miss-Aliza.git
 
-PUBLIC BOT GROUP 👇: https://chat.whatsapp.com/KeEMhqBW644IN46PYsGDWf
+PUBLIC BOT GROUP 👇: https://chat.whatsapp.com/BnaB7uA4dQF8Qhcac3bGWE
 
 
 `}) 
